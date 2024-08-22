8b85dfe5b8214b4635030e98ce2af369b7c8cef9
mohammed7733 <mohammedessam7733@gmail.comgit config --global user.email mohammedessam7733@gmail.com>
2024-08-22 21:06:41 +0300

    Refactor tests

--- expensereport-ut/UnitTest1.cs
+++ expensereport-ut/UnitTest1.cs
@@ -31,42 +31,63 @@ namespace Tests
             Assert.True(breakfast.IsMeal());
             Assert.Equal(isOverExpense, breakfast.IsOverExpense());
         }
-        
+
+        [Theory]
+        [InlineData(2900, false)]
+        [InlineData(3000, false)]
+        [InlineData(3100, true)]
+        public void NewLunch_InitiatedAsExpected(int amount, bool isOverExpense)
+        {
+            var lunch = new Lunch(amount);
+            Assert.Equal(amount, lunch.Amount);
+            Assert.Equal("Lunch", lunch.Name());
+            Assert.True(lunch.IsMeal());
+            Assert.Equal(isOverExpense, lunch.IsOverExpense());
+        }
+
+        [Theory]
+        [InlineData(4900, false)]
+        [InlineData(5000, false)]
+        [InlineData(5100, true)]
+        public void NewDinner_InitiatedAsExpected(int amount, bool isOverExpense)
+        {
+            var dinner = new Dinner(amount);
+            Assert.Equal(amount, dinner.Amount);
+            Assert.Equal("Dinner", dinner.Name());
+            Assert.True(dinner.IsMeal());
+            Assert.Equal(isOverExpense, dinner.IsOverExpense());
+        }
+
+        [Fact]
+        public void NewCarRental_InitiatedAsExpected()
+        {
+            var carRental = new CarRental(70);
+            Assert.Equal(70, carRental.Amount);
+            Assert.Equal("Car Rental", carRental.Name());
+            Assert.False(carRental.IsMeal());
+            Assert.False(carRental.IsOverExpense());
+        }
+
         [Fact]
         public void PrintReport_WithExpenses_WorkAsExpected()
         {
             StringWriter writer = new StringWriter();
             Console.SetOut(writer);
             var expenseReport = new ExpenseReport();
-            //TODO: I can separate tests (tests for breakfast, others for dinner, and so on ..)
             var expenses = new List<Expense>
             {
                 new Breakfast(900),
-                new Breakfast(1000),
-                new Breakfast(1100),
-                new Dinner(4900),
-                new Dinner(5000),
                 new Dinner(5100),
-                new CarRental(2000),
-                new Lunch(2900),
-                new Lunch(3000),
-                new Lunch(3100),
+                new CarRental(2000)
             };
             var dateTime = DateTime.Now;
             expenseReport.PrintReport(new Expenses(expenses));
             Assert.Equal($"Expenses {dateTime}\n" +
                          $"Breakfast\t900\t \n" +
-                         $"Breakfast\t1000\t \n" +
-                         $"Breakfast\t1100\tX\n" +
-                         $"Dinner\t4900\t \n" +
-                         $"Dinner\t5000\t \n" +
                          $"Dinner\t5100\tX\n" +
                          $"Car Rental\t2000\t \n" +
-                         $"Lunch\t2900\t \n" +
-                         $"Lunch\t3000\t \n" +
-                         $"Lunch\t3100\tX\n" +
-                         $"Meal expenses: 27000\n" +
-                         $"Total expenses: 29000\n", writer.ToString());
+                         $"Meal expenses: 6000\n" +
+                         $"Total expenses: 8000\n", writer.ToString());
         }
     }
 }
